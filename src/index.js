fe680087f0c47ca76e4cd6ce460791f7a79d0cd4
Vasco Silva <vasco.s.rebolo@gmail.com>
2016-10-04 18:24:55 +0100

    Add video list and video list item components, use youtube api to fetch videos

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyBpUg7n88Wdkf3muaYn8SgwLehdeBh-0O0';
 
-// Create a new component. This component should produce
-// some html
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-// take this component's generated HTML and put it
-// on the page (in the DOM)
+    this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+      this.setState({videos});
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(<App />, document.querySelector('.container'));
