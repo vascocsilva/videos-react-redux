46341a2f4958b092891fea01ae919b57c08c3209
Vasco Silva <vasco.s.rebolo@gmail.com>
2016-10-05 21:58:16 +0100

    Add VideoDetails component (youtube video embed section)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBpUg7n88Wdkf3muaYn8SgwLehdeBh-0O0';
 
@@ -22,6 +23,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );
