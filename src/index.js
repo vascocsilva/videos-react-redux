f3b8bfab64a7496fd464a42f96cebaabe17c4bd4
Vasco Silva <vasco.s.rebolo@gmail.com>
2016-10-06 13:56:34 +0100

    Change debounce timer, Change readme

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ class App extends Component {
   }
 
   render() {
-    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 500);
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
 
     return (
       <div>
